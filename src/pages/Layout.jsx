f41a3b2071b12beb27d0d4d08cb75c446a328c13
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2024-05-26 09:22:37 +0530

    Fff

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -9,13 +9,13 @@ const Layout = () => {
   return (
     <div className="h-[100vh]">
       <Nav />
-      <div className='h-[90vh] overflow-y-auto'>
+      <div className="h-[90vh] overflow-y-auto">
         <Side />
-        <p className='p-4 font-bold text-xl my-2'>Shop by Category</p>
-        <div className='w-full overflow-x-auto'>
+        <p className="p-4 font-bold text-xl my-2">Shop by Category</p>
+        <div className="w-full overflow-x-auto">
           <Category />
         </div>
-        <p className='p-4 font-bold text-xl my-2'>Shop Our Latest Styles!</p>
+        <p className="p-4 font-bold text-xl my-2">Shop Our Latest Styles!</p>
         <div>
           <FeedProduct />
         </div>
