dc2c3ce4f96ce8cf8ab4378826e2efad5a1f43fb
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2024-06-01 10:58:19 +0530

    done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Category = () => {
   const [category, setcategory] = useState([]);
+  const [rcat, setrcat] = useState("");
   const url = import.meta.env.VITE_BACKEND;
-
+  const navigate = useNavigate();
   const getCategories = async () => {
     try {
       const { data } = await axios.get(`${url}/admin/category`);
@@ -18,13 +20,24 @@ const Category = () => {
     getCategories();
   }, []);
 
+  const click = async (id) => {
+    setrcat(id);
+    navigate("/products", { state: { categorys: id } });
+  };
 
   return (
     <div className=" grid grid-cols-3 md:grid-cols-5 gap-3 md:gap-5 flex-wrap md:px-36 justify-center items-center">
       {category?.map((item, index) => (
-        <div key={index} className=" flex flex-col items-center rounded-md gap-2">
+        <div
+          onClick={() => click(item._id)}
+          key={index}
+          className=" flex flex-col items-center rounded-md gap-2"
+        >
           <div className="  w-24 h-24 aspect-square rounded-full relative overflow-hidden">
-            <img src={item?.image} className="object-cover aspect-square rounded-md" />
+            <img
+              src={item?.image}
+              className="object-cover aspect-square rounded-md"
+            />
           </div>
           <p className="flex justify-center text-center text-black text-sm font-semibold">
             {item?.name}
