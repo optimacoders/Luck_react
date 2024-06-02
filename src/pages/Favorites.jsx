153e1d3fac3ee3c85d6008cc8b09b00bf87771bf
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2024-06-02 12:52:47 +0530

    done

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { getRequest } from "../utils/Apihelpers"
-import ProductCard from '../components/ProductCard';
+import React, { useEffect, useState } from "react";
+import { getRequest } from "../utils/Apihelpers";
+import ProductCard from "../components/ProductCard";
 import ProductCardSkeleton from "../skeletons/ProductCardSkeleton";
 
-
 function Favorites() {
-    const [data, setdata] = useState()
-    const [loader, setloader] = useState(false)
+  const [data, setdata] = useState();
+  const [loader, setloader] = useState(false);
 
-    const getdata = async () => {
-        setloader(true)
-        const res = await getRequest(true, '/liked');
-        if (res.status) {
-            setdata(res.favourites)
-        }
-        setloader(false)
+  const getdata = async () => {
+    setloader(true);
+    const res = await getRequest(true, "/liked");
+    if (res.status) {
+      setdata(res.favourites?.data);
     }
+    setloader(false);
+  };
 
-    useEffect(() => {
-        getdata()
-    }, [])
+  useEffect(() => {
+    getdata();
+  }, []);
 
-    return (
-        <div className=' p-3 overflow-y-auto h-full'>
-            <p className=' text-lg font-medium'>Your Favorites</p>
-            <div className=' grid grid-cols-2 md:grid-cols-4'>
-                {loader ? <><ProductCardSkeleton />
-                    <ProductCardSkeleton />
-                    <ProductCardSkeleton />
-                    <ProductCardSkeleton />
-                </> :
-                    data?.map((item) => {
-                        return <div key={item._id}>
-                            <ProductCard data={item.product} />
-                        </div>
-                    })
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className=" p-3 overflow-y-auto h-full">
+      <p className=" text-lg font-medium">Your Favorites</p>
+      <div className=" grid grid-cols-2 md:grid-cols-4">
+        {loader ? (
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        ) : (
+          data?.map((item) => {
+            return (
+              <div key={item._id}>
+                <ProductCard data={item.product} />
+              </div>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Favorites
+export default Favorites;
